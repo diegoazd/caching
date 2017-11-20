038afa0e382a29d74ad4e42d6756e5aaab31abd2
Diego <diegoazd@hotmail.com>
2017-11-19 21:14:02 -0600

    add logger

--- src/main/java/hello/SimpleBookRepository.java
+++ src/main/java/hello/SimpleBookRepository.java
@@ -1,25 +1,39 @@
 package hello;
 
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
 public class SimpleBookRepository implements BookRepository {
 
-    @Override
-    @Cacheable("books")
-    public Book getByIsbn(String isbn) {
-        simulateSlowService();
-        return new Book(isbn, "Some book");
+    private static Logger log = Logger.getAnonymousLogger();
+    Map<String, Book> cache;
+
+    SimpleBookRepository() {
+        cache = new ConcurrentHashMap<>();
     }
+
+    @Override
+    public Book getByIsbn(String isbn) {
+        if(cache.containsKey(isbn)) {
+            log.info("Getting key");
+            return cache.get(isbn);
+        }
+        simulateSlowService();
+        Book book = new Book(isbn, "Some book");
+        cache.put(isbn, book);
+        return book;
+    }
+
     // Don't do this at home
     private void simulateSlowService() {
         try {
+            log.info("slow functiooooon");
             long time = 3000L;
             Thread.sleep(time);
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         }
     }
-
 }
--- src/test/java/hello/SimpleBookRepositoryTest.java
+++ src/test/java/hello/SimpleBookRepositoryTest.java
@@ -4,35 +4,20 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {Application.class})
 public class SimpleBookRepositoryTest {
 
-    @Autowired
     BookRepository bookRepository;
 
-    @Test(timeout = 3500)
-    public void shouldUseCacheForMultipleRequests() {
-        bookRepository.getByIsbn("81231211AD1");
-        bookRepository.getByIsbn("81231211AD1");
-        bookRepository.getByIsbn("81231211AD1");
-        bookRepository.getByIsbn("81231211AD1");
-        bookRepository.getByIsbn("81231211AD1");
-        bookRepository.getByIsbn("81231211AD1");
-        bookRepository.getByIsbn("81231211AD1");
-
-        assertTrue(2 > 1);
+    @Before
+    public void setUp() {
+        bookRepository = new SimpleBookRepository();
     }
 
     @Test(timeout = 3500)
-    public void shouldUseCacheNoCacheForMultipleRequests() {
-        BookRepository bookRepository = new SimpleBookRepository();
+    public void shouldUseCacheForMultipleRequests() {
         bookRepository.getByIsbn("81231211AD1");
         bookRepository.getByIsbn("81231211AD1");
         bookRepository.getByIsbn("81231211AD1");
